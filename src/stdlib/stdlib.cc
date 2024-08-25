876aecd92398e00a897378c3859d9de0d59587c6
= <dev.258.edilson@gmail.com>
2024-08-25 09:37:43 +0200

    feat: impl Math object

--- src/stdlib/stdlib.cc
+++ src/stdlib/stdlib.cc
@@ -19,9 +19,25 @@ std::shared_ptr<Type> StandardLibrary::makeConsole()
     return std::make_shared<TypeObject>(consoleFields, "Console");
 }
 
+std::shared_ptr<Type> StandardLibrary::makeMath()
+{
+    std::unordered_map<std::string, std::shared_ptr<Type>> mathFields;
+
+    // pow function
+    bool powIsVarArgs = false;
+    std::vector<std::shared_ptr<Type>> powParams{std::make_shared<Type>(TypeKind::Number),
+                                                 std::make_shared<Type>(TypeKind::Number)};
+    auto powReturnType = std::make_shared<Type>(TypeKind::Number);
+    auto powFunction = std::make_shared<TypeFunction>(powIsVarArgs, powReturnType, powParams);
+    mathFields["pow"] = powFunction;
+
+    return std::make_shared<TypeObject>(mathFields, "Math");
+}
+
 std::unordered_map<std::string, std::shared_ptr<Type>> StandardLibrary::GetBuiltins()
 {
     std::unordered_map<std::string, std::shared_ptr<Type>> builtins;
     builtins["console"] = makeConsole();
+    builtins["Math"] = makeMath();
     return builtins;
 }
--- src/stdlib/stdlib.h
+++ src/stdlib/stdlib.h
@@ -9,6 +9,7 @@ class StandardLibrary
 {
   private:
     static std::shared_ptr<Type> makeConsole();
+    static std::shared_ptr<Type> makeMath();
 
   public:
     static std::unordered_map<std::string, std::shared_ptr<Type>> GetBuiltins();
